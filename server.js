4a03a6916fe95848f73251af4a5793d225f5755c
Andras Varga <vargaae@hotmail.com>
2022-01-01 04:06:24 +0100

    adding port

--- server.js
+++ server.js
@@ -45,6 +45,6 @@ app.put("/image", (req, res) => {
   image.handleImage(req, res, db);
 });
 
-app.listen(3000, () => {
-  console.log("app is running on port 3000");
-});
+app.listen(process.env.PORT || 3000, ()=> { 
+  console.log('app is running on port ${process.env.PORT}') 
+})
